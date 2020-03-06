327b1078711d54dd24cdca9cf85a0323bf508007
gulraizfathima2 <gulraiz.fathima@outlook.com>
2020-03-06 06:34:52 +0000

    first update v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,20 @@ if [ "$color" == "black" ]
 
 then 
 	cp /opt/gul.html /var/www/html/index.html
+	cp /opt/docker.png /var/www/html/
 	httpd -DFOREGROUND
 elif [ "$color" == "red" ]
 
 then 
 	cp /opt/red.html /var/www/html/index.html
+	cp /opt/docker.png /var/www/html/
 	httpd -DFOREGROUND
 
 elif [ "$color" == "green" ]
 
 then
         cp /opt/green.html /var/www/html/index.html
+	cp /opt/docker.png /var/www/html/
         httpd -DFOREGROUND
 
 else 
